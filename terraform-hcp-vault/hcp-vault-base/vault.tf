72f2954bdabdafd29d1a2ce6d0fbd117f61dbdd3
Sébastien Braun <sebbraun@gmail.com>
2022-06-23 17:09:53 +0200

    corrected output

--- terraform-hcp-vault/hcp-vault-base/vault.tf
+++ terraform-hcp-vault/hcp-vault-base/vault.tf
@@ -15,7 +15,7 @@ resource "hcp_vault_cluster_admin_token" "token" {
   cluster_id = var.cluster_id
 }
 
-output "public_endoing_url" {
+output "public_endpoint_url" {
   value = hcp_vault_cluster.hcp_vault.vault_public_endpoint_url
 }
 
